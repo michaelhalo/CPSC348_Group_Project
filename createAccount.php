6d7b0ff0e32501e63e3876622eff1aa99dc0c7a6
Michelle Booth <mtb.0011@yahoo.com>
2017-12-06 13:04:37 -0500

    Added Create Account

---
+++ createAccount.php
@@ -0,0 +1,52 @@
+<html>
+<body>
+<head>
+<link href="gp.css" rel="stylesheet"/>
+</head>
+<form action="results.php" method="post">
+Please Enter Your Name:
+<input type="text" name"name" id="name"><br>
+<label for ="classNum">Select your number of classes:</label><br>
+<select>
+<option value="3">3</option><br/>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+</select>
+<br>
+<br>
+Enter Class Name:<br>
+<input type ="text" name="className" id="className1"><br>
+Enter Class Time:<br>
+<input type ="text" name="classTime" id="classTimeH1">:
+<input type ="text" name="classTime" id="classTimeM1">
+<br><br>
+Enter Class Name:<br>
+<input type ="text" name="className" id="className2"><br>
+Enter Class Time:<br>
+<input type ="text" name="classTime" id="classTimeH2">:
+<input type ="text" name="classTime" id="classTimeM2">
+<br><br>
+Enter Class Name:<br>
+<input type ="text" name="className" id="className3"><br>
+Enter Class Time:<br>
+<input type ="text" name="classTime" id="classTimeH3">:
+<input type ="text" name="classTime" id="classTimeM3">
+<br><br>
+Enter Class Name:<br>
+<input type ="text" name="className" id="className4"><br>
+Enter Class Time:<br>
+<input type ="text" name="classTime" id="classTimeH4">:
+<input type ="text" name="classTime" id="classTimeM4">
+<br><br>
+Enter Class Name:<br>
+<input type ="text" name="className" id="className5"><br>
+Enter Class Time:<br>
+<input type ="text" name="classTime" id="classTimeH5">:
+<input type ="text" name="classTime" id="classTimeM5">
+<br><br>
+Enter Class Name:<br>
+<input type ="text" name="className" id="className6"><br>
+Enter Class Time:<br>
+<input type ="text" name="classTime" id="classTimeH6">:
+<input type ="text" name="classTime" id="classTimeM6">
