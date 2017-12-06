1a8716c33073621ea6afc51c363c24def24f657f
Michelle Booth <mtb.0011@yahoo.com>
2017-12-06 14:49:36 -0500

    Changed Input Box to Select Box

--- createAccount.php
+++ createAccount.php
@@ -18,8 +18,31 @@ Please Enter Your Name:
 Enter Class Name:<br>
 <input type ="text" name="className" id="className1"><br>
 Enter Class Time:<br>
-<input type ="text" name="classTime" id="classTimeH1">:
-<input type ="text" name="classTime" id="classTimeM1">
+<select>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option><br/>
+<option value="9">9</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+</select>
+:
+<select>
+<option value="00">00</option><br/>
+<option value="15">15</option><br/>
+<option value="30">30</option><br/>
+<option value="45">45</option><br/>
+</select>
+ <select>
+<option value="am">am</option><br/>
+<option value="pm">pm</option><br/>
+</select>
 <br><br>
 Enter Class Name:<br>
 <input type ="text" name="className" id="className2"><br>
--- profile.php
+++ profile.php
@@ -7,6 +7,8 @@
 </div>
 <br>
 <img src="http://gazettereview.com/wp-content/uploads/2016/03/facebook-avatar.jpg" style="width:200px; height:150px;">
+
+<p id="name"> User McUser</p>
 </head>
 </script>
 </body>
