529a1c6e5e10f4106e35432128eb1a5143ce8185
Michelle Booth <mtb.0011@yahoo.com>
2017-12-06 15:08:21 -0500

    Changed input to drop / need to fix name input

--- createAccount.php
+++ createAccount.php
@@ -4,14 +4,130 @@
 <link href="gp.css" rel="stylesheet"/>
 </head>
 <form action="results.php" method="post">
-Please Enter Your Name:
-<input type="text" name"name" id="name"><br>
 <label for ="classNum">Select your number of classes:</label><br>
 <select>
 <option value="3">3</option><br/>
 <option value="4">4</option>
 <option value="5">5</option>
 <option value="6">6</option>
+</select>
+<br>
+<br>
+
+Enter Class Name:<br>
+<input type ="text" name="className" id="className1"><br>
+Enter Class Time:<br>
+<select>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option><br/>
+<option value="9">9</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+</select>
+:
+<select>
+<option value="00">00</option><br/>
+<option value="15">15</option><br/>
+<option value="30">30</option><br/>
+<option value="45">45</option><br/>
+</select>
+ <select>
+<option value="am">am</option><br/>
+<option value="pm">pm</option><br/>
+</select>
+<br><br>
+Enter Class Name:<br>
+<input type ="text" name="className" id="className1"><br>
+Enter Class Time:<br>
+<select>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option><br/>
+<option value="9">9</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+</select>
+:
+<select>
+<option value="00">00</option><br/>
+<option value="15">15</option><br/>
+<option value="30">30</option><br/>
+<option value="45">45</option><br/>
+</select>
+ <select>
+<option value="am">am</option><br/>
+<option value="pm">pm</option><br/>
+</select>
+<br><br>
+Enter Class Name:<br>
+<input type ="text" name="className" id="className1"><br>
+Enter Class Time:<br>
+<select>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option>
+<option value="9">9</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+</select>
+:
+<select>
+<option value="00">00</option><br/>
+<option value="15">15</option><br/>
+<option value="30">30</option><br/>
+<option value="45">45</option><br/>
+</select>
+ <select>
+<option value="am">am</option><br/>
+<option value="pm">pm</option><br/>
+</select>
+<br> <br>
+Enter Class Name:<br>
+<input type ="text" name="className" id="className1"><br>
+Enter Class Time:<br>
+<select>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option><br/>
+<option value="9">9</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+</select>
+:
+<select>
+<option value="00">00</option><br/>
+<option value="15">15</option><br/>
+<option value="30">30</option><br/>
+<option value="45">45</option><br/>
+</select>
+ <select>
+<option value="am">am</option><br/>
+<option value="pm">pm</option><br/>
 </select>
 <br>
 <br>
@@ -43,33 +159,34 @@ Enter Class Time:<br>
 <option value="am">am</option><br/>
 <option value="pm">pm</option><br/>
 </select>
-<br><br>
+<br>
+<br>
 Enter Class Name:<br>
-<input type ="text" name="className" id="className2"><br>
+<input type ="text" name="className" id="className1"><br>
 Enter Class Time:<br>
-<input type ="text" name="classTime" id="classTimeH2">:
-<input type ="text" name="classTime" id="classTimeM2">
-<br><br>
-Enter Class Name:<br>
-<input type ="text" name="className" id="className3"><br>
-Enter Class Time:<br>
-<input type ="text" name="classTime" id="classTimeH3">:
-<input type ="text" name="classTime" id="classTimeM3">
-<br><br>
-Enter Class Name:<br>
-<input type ="text" name="className" id="className4"><br>
-Enter Class Time:<br>
-<input type ="text" name="classTime" id="classTimeH4">:
-<input type ="text" name="classTime" id="classTimeM4">
-<br><br>
-Enter Class Name:<br>
-<input type ="text" name="className" id="className5"><br>
-Enter Class Time:<br>
-<input type ="text" name="classTime" id="classTimeH5">:
-<input type ="text" name="classTime" id="classTimeM5">
-<br><br>
-Enter Class Name:<br>
-<input type ="text" name="className" id="className6"><br>
-Enter Class Time:<br>
-<input type ="text" name="classTime" id="classTimeH6">:
-<input type ="text" name="classTime" id="classTimeM6">
+<select>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option><br/>
+<option value="9">9</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+</select>
+:
+<select>
+<option value="00">00</option><br/>
+<option value="15">15</option><br/>
+<option value="30">30</option><br/>
+<option value="45">45</option><br/>
+</select>
+ <select>
+<option value="am">am</option><br/>
+<option value="pm">pm</option><br/>
+</select>
+
