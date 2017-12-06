f12c472aa1a70303aa5392e142c2f3730af6e302
Michelle Booth <mtb.0011@yahoo.com>
2017-12-06 13:47:08 -0500

    Added Banner to Profile

--- profile.php
+++ profile.php
@@ -2,6 +2,11 @@
 <body>
 <head>
 <link href="gp.css" rel="stylesheet"/>
+<div class="banner">
+<img class="banner" src="banner.png" style="width:100%; height:50px;">
+</div>
+<br>
+<img src="http://gazettereview.com/wp-content/uploads/2016/03/facebook-avatar.jpg" style="width:200px; height:150px;">
 </head>
 </script>
 </body>
