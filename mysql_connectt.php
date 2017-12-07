211dabb2a451ab629f1d9229d015cebf8a2798db
Leonard <llopez4@mail.umw.edu>
2017-12-06 23:28:11 -0500

    added my login info to access the tables

--- mysql_connectt.php
+++ mysql_connectt.php
@@ -1,8 +1,8 @@
 <?php
-DEFINE ('DB_USER', 'mbooth');
-DEFINE ('DB_PASSWORD', 'Mb467209');
+DEFINE ('DB_USER', 'lopez4');
+DEFINE ('DB_PASSWORD', 'Ll949608');
 DEFINE ('DB_HOST', '127.0.0.1');
-DEFINE ('DB_NAME', 'F17-CPSC348-01_mbooth');
+DEFINE ('DB_NAME', 'F17-CPSC348-01_llopez4');
  
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 	OR die('Could not connect to MySQL: ' .
