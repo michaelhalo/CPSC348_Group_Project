1ef8f00900436c5fc848724ced770cc1d7454f17
Michelle Booth <mtb.0011@yahoo.com>
2017-12-06 22:55:20 -0500

    Banner/Home button added to createAccount

--- createAccount.php
+++ createAccount.php
@@ -7,7 +7,7 @@
 <img class="banner" src="banner.png" style="width:100%; height:50px;">
 </div>
 <br>
-<div id="banner">
+<div id="banner2">
 <a href="profile.php"><font color="white">Home</a><u>&nbsp &nbsp Friends &nbsp &nbsp Free Time</font></u>
 </div>
 <label for ="classNum">Select your number of classes:</label><br>
