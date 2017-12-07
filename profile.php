c36eebecc8e5e1f87b603020ead2cdbddb7a2790
Michelle Booth <mtb.0011@yahoo.com>
2017-12-06 22:03:59 -0500

    Added Banner to Profile

--- profile.php
+++ profile.php
@@ -6,8 +6,10 @@
 <img class="banner" src="banner.png" style="width:100%; height:50px;">
 </div>
 <br>
-<img src="http://gazettereview.com/wp-content/uploads/2016/03/facebook-avatar.jpg" style="width:200px; height:150px;">
-
+<img src="http://profile.actionsprout.com/default.jpeg" style="width:150px; height:150px;">
+<div id="banner">
+<a href="profile.php"><font color="white">Home</a><u>&nbsp &nbsp Friends &nbsp &nbsp Free Time
+</div>
 <p id="name"> User McUser</p>
 </head>
 </script>
