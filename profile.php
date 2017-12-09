5d26d8f7eafcabd8cef0795d5d9900a6540cb185
Michelle Booth <mtb.0011@yahoo.com>
2017-12-08 23:26:03 -0500

    Fixed profile page

--- profile.php
+++ profile.php
@@ -27,25 +27,25 @@ require_once('.CPSC348_Group_Project/mysql_connect.php');
 
 /* Handle posted data */
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-	POST$query = "INSERT INTO classes (class1, class2, class3,
+	$query = "INSERT INTO classes (class1, class2, class3,
 		class2class4, class5, class6) VALUES (?, ?, ?, ?, ?, ?)";
-	VALUES$stmt = $dbc->prepare($query);
-	queryif (!$stmt) {
-		+queryifecho $dbc->error;
-		error}
+	$stmt = $dbc->prepare($query);
+	if (!$stmt) {
+	echo $dbc->error;
+	}
 
-	error$stmt->bind_param("sssss",$_POST['class1'],
-			sssss$_POST['class2'], $_POST['class3'], $_POST['class4'], $_POST['class5'], $_POST['class6']);
-	class6$stmt->execute();
-	execute$affected_rows = mysqli_stmt_affected_rows($stmt);
-	stmtif($affected_rows == 1){
+	$stmt->bind_param("sssss",$_POST['class1'],
+		$_POST['class2'], $_POST['class3'], $_POST['class4'], $_POST['class5'], $_POST['class6']);
+	$stmt->execute();
+	$affected_rows = mysqli_stmt_affected_rows($stmt);
+	if ($affected_rows == 1){
 
 
-		stmtifmysqli_stmt_close($stmt);
+	mysqli_stmt_close($stmt);
 
 #mysqli_close($dbc);
 
-		dbc}
+	}
 }
 
 // Create a query for the database
@@ -57,7 +57,7 @@ $response = $dbc->query($query);
 // If the query executed properly proceed
 if($response->num_rows > 0){
 
-	num_rowsecho '<table align="left"
+	echo '<table align="left"
 		alignllspacing="5" cellpadding="8">
 		+
 		alignllspacing<tr><td align="left"><b>Class 1</b></td>
